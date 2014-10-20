6f2cae07e53ccbe16997ffad926690eaf4d41c72
tabbabi <marwen.tabbabi@gmail;com>
2014-10-20 14:22:15 +0100

    fix bug

--- web/app.php
+++ web/app.php
@@ -1,6 +1,6 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
+//use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
@@ -8,10 +8,13 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
+if(function_exists('apc_store')) {
+    $loader = new ApcClassLoader('KarisService', $loader);
+    $loader->unregister();
+    $loader->register(true);
+}
+
+
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
